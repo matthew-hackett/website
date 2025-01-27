a77f7c933677c50b12e0ee27f960805c282f3037
Matthew Hackett <195298367+matthew-hackett@users.noreply.github.com>
2025-01-27 17:54:19 +1100

    feat: new header, needs to add in new links

--- src/components/core/header/Header.tsx
+++ src/components/core/header/Header.tsx
@@ -1,51 +1,135 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import styles from "./Header.module.css";
 import Link from "next/link";
 
 type Props = {};
 
 export default function Header({}: Props) {
+    const [menuOn, setMenuOn] = useState(false);
+    const [menuClass, setMenuClass] = useState("");
+    const [iconClass, setIconClass] = useState("");
+    const toggleMenu = () => {
+        setMenuOn(!menuOn);
+    };
+
+    useEffect(() => {
+        setMenuClass(menuOn ? styles.showMenu : "");
+        setIconClass(menuOn ? styles.showIcon : "");
+    }, [menuOn]);
+
     return (
-        <header className={styles.container}>
-            <div className={styles.navbar}>
-                <div className={styles.logoDiv}>
-                <Link href="/" className={styles.link}><img
-                    className={styles.logo}
-                    src="/static/images/logo.svg"
-                /></Link>
+        <>
+        <div className={styles.pad}></div>
+        <header className={styles.header}>
+            <nav className={styles.nav + " " + styles.container}>
+                <div className={styles.navData}>
+                    <Link
+                        href="/"
+                        className={styles.navLogo}
+                    >
+                        <img
+                            className={styles.logo}
+                            src="/static/images/logo.svg"
+                        />
+                    </Link>
+
+                    <div
+                        className={`${styles.navToggle} ${iconClass}`}
+                        onClick={toggleMenu}
+                    >
+                        <img
+                            className={styles.navBurger}
+                            src="/static/images/menu-line.svg"
+                        ></img>
+                        <img
+                            className={styles.navClose}
+                            src="/static/images/close-line.svg"
+                        ></img>
+                    </div>
                 </div>
-                
-                <nav>
-                    <ul>
+                <div className={`${styles.navMenu} ${menuClass}`}>
+                    <ul className={styles.navList}>
                         <li>
                             <Link
                                 href="/"
-                                className={styles.link}
-                            ><span>Home</span>
-                                
+                                className={styles.navLink}
+                            >
+                                Home
                             </Link>
                         </li>
-                        <li>
-                            <Link
-                                href="/articles"
-                                className={styles.link}
-                            >
-                                <span>
-                                    Articles
-                                </span>
-                            </Link>
+
+                        {/* DROPDOWN 1 */}
+                        <li className={styles.dropdownItem}>
+                            <div className={styles.navLink}>
+                                Blog <img className={styles.dropdownArrow} src="/static/images/arrow-down-s-line.svg"></img>
+                            </div>
+
+                            <ul className={styles.dropdownMenu}>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                            </ul>
                         </li>
-                        <li>
-                            <Link
-                                href="/puzzles"
-                                className={styles.link}
-                            >
-                                <span>Puzzles</span>
-                            </Link>
+
+                        {/* DROPDOWN 2 */}
+                        <li className={styles.dropdownItem}>
+                            <div className={styles.navLink}>
+                                Puzzles <img className={styles.dropdownArrow} src="/static/images/arrow-down-s-line.svg"></img>
+                            </div>
+                            <ul className={styles.dropdownMenu}>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href=""
+                                        className={styles.dropdownLink}
+                                    >
+                                        Home
+                                    </Link>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </header>
+        </>
     );
 }
